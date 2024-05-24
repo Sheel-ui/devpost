6f5277a78f20588b5c4bf506e3d71ea02319fa19
Sheel Taskar <sheeltaskar64@gmail.com>
2024-05-24 19:13:19 +0530

    Add server actions for data manipulation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,7 @@
 "use server";
 
-import * as auth from "@/auth";
-
-export async function signIn() {
-	return auth.signIn("github");
-}
-
-export async function signOut() {
-	return auth.signOut();
-}
+export { signIn } from "./signIn";
+export { signOut } from "./signOut";
+export { createComment } from "./createComment";
+export { createPost } from "./createPost";
+export { createTopic } from "./createTopic";
