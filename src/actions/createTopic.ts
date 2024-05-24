d63b68a50778b468282082257fba6945ab53f152
Sheel Taskar <sheeltaskar64@gmail.com>
2024-05-24 23:47:02 +0530

    Added useFormSate for validation

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -2,19 +2,40 @@
 
 import { z } from "zod";
 
+interface createTopicFormState {
+	errors: {
+		name?: string[];
+		description?: string[];
+	};
+}
+
 const createTopicSchema = z.object({
-    name: z.string().min(3).regex(/^[a-z-]+$/, {message:'Must be lower case characters with hyphens'}),
-    description: z.string().min(10),
-})
+	name: z
+		.string()
+		.min(3)
+		.regex(/^[a-z-]+$/, {
+			message: "Must be lower case characters with hyphens",
+		}),
+	description: z.string().min(10),
+});
 
 // reval homepage
-export async function createTopic(formData: FormData) {
-    const result = createTopicSchema.safeParse({
-        name: formData.get('name'),
-        description: formData.get('description')
-    })
+export async function createTopic(
+	formState: createTopicFormState,
+	formData: FormData
+): Promise<createTopicFormState> {
+	const result = createTopicSchema.safeParse({
+		name: formData.get("name"),
+		description: formData.get("description"),
+	});
 
-    if (!result.success){
-        console.log(result.error.flatten().fieldErrors);
-    }
+	if (!result.success) {
+		return {
+			errors: result.error.flatten().fieldErrors,
+		};
+	}
+
+	return {
+		errors: {},
+	};
 }
--- src/components/topics/topicCreate.tsx
+++ src/components/topics/topicCreate.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
 	Textarea,
 	Button,
@@ -7,15 +9,20 @@ import {
 	PopoverContent,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import { useFormState } from "react-dom";
 
 export default function TopicCreate() {
+	const [formState, action] = useFormState(actions.createTopic, {
+		errors: {},
+	});
+
 	return (
 		<Popover placement="left">
 			<PopoverTrigger>
 				<Button color="primary">Create a Topic</Button>
 			</PopoverTrigger>
 			<PopoverContent>
-				<form action={actions.createTopic}>
+				<form action={action}>
 					<div className="flex flex-col gap-4 p-4 w-80">
 						<h3 className="text-lg">Create a Topic</h3>
 						<Input
@@ -23,12 +30,18 @@ export default function TopicCreate() {
 							name="name"
 							labelPlacement="outside"
 							placeholder="Name"
+							isInvalid={!!formState.errors.name}
+							errorMessage={formState.errors.name?.join(", ")}
 						/>
 						<Textarea
 							label="Description"
 							name="description"
 							labelPlacement="outside"
 							placeholder="Describe your topic"
+							isInvalid={!!formState.errors.description}
+							errorMessage={formState.errors.description?.join(
+								", "
+							)}
 						/>
 						<Button type="submit">Submit</Button>
 					</div>
