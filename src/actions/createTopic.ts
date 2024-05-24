1629e7dd77ba576230075adcf81bed1fe585a07b
Sheel Taskar <sheeltaskar64@gmail.com>
2024-05-24 23:26:42 +0530

    Add zod for validation

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -1,4 +1,20 @@
 "use server";
 
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z-]+$/, {message:'Must be lower case characters with hyphens'}),
+    description: z.string().min(10),
+})
+
 // reval homepage
-export async function createTopic(formData: FormData) {}
+export async function createTopic(formData: FormData) {
+    const result = createTopicSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description')
+    })
+
+    if (!result.success){
+        console.log(result.error.flatten().fieldErrors);
+    }
+}
